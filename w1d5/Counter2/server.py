9ce819d4b382a47f6e21f910e286e1bc0a646e3c
John Doe <johndoe@example.com>
2022-11-01 12:38:33 -0700

    Import session

--- w1d5/Counter2/server.py
+++ w1d5/Counter2/server.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect
 app = Flask(__name__)
+app.secret_key = "rootroot"
 
 
 @app.route('/')
 def index():
-    return "Hello World"
 
     if 'counter' in session:
         session['counter'] += 1
-        session['pagevisits'] += 1
-        return render_template('index.html', x=session['counter'], page)
+    else:  # else have to have colons after them
+        session['counter'] = 1
+    return render_template('index.html')
 
-    if __name__ == "__main__":
-        app.run(debug=True)
-#Virtual environment failed to be created
+
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# Virtual environment failed to be created
 # Create the virtual environment
 # 	a. in terminal
 # 	b. pip3 install pipenv —> press enter
@@ -21,5 +30,5 @@ def index():
 # 	d. open VSC folder (counter) —> press command-shift-c
 # 	e. python3 -m pipenv shell —> press enter to activate virtual environment
 # ERROR:: --system is intended to be used for pre-existing Pipfile installation, not installation of specific packages. Aborting.
-# boydlever@Boyds-MacBook-Air Counter % 
+# boydlever@Boyds-MacBook-Air Counter %
 # 	 f. python3 server.py —> Launch the server after server.py is set up
