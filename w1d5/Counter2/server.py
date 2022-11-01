b4537e9e45e665ad125d150cf74e32ceb71d537f
John Doe <johndoe@example.com>
2022-11-01 12:41:00 -0700

    Added comments

--- w1d5/Counter2/server.py
+++ w1d5/Counter2/server.py
@@ -20,15 +20,3 @@ def destroy():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# Virtual environment failed to be created
-# Create the virtual environment
-# 	a. in terminal
-# 	b. pip3 install pipenv —> press enter
-# 	c. pip3 list —> press enter to see the version of pipenv installed
-# 	d. open VSC folder (counter) —> press command-shift-c
-# 	e. python3 -m pipenv shell —> press enter to activate virtual environment
-# ERROR:: --system is intended to be used for pre-existing Pipfile installation, not installation of specific packages. Aborting.
-# boydlever@Boyds-MacBook-Air Counter %
-# 	 f. python3 server.py —> Launch the server after server.py is set up
