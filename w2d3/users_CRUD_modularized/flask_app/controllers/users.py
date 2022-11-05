0f774c0d9a8e159f8a546849f995df5c6de6b505
Boyd Lever <boydlever@stanfordhacker.com>
2022-11-04 21:17:26 -0700

    users CRUD modularized

--- w2d3/users_CRUD_modularized/flask_app/controllers/users.py
+++ w2d3/users_CRUD_modularized/flask_app/controllers/users.py
@@ -8,19 +8,56 @@ from flask_app import app
 def index():
     return render_template('create.html')
 
-@app.route('/create/users', methods=['POST']) #like the middle man, it will get the information from create
-def create_user(): #the form from '/' will be sent to this route because it is a post route
+
+# like the middle man, it will get the information from create
+@app.route('/create/users', methods=['POST'])
+def create_user():  # the form from '/' will be sent to this route because it is a post route
     data = {
         "first_name": request.form['first_name'],
         "last_name": request.form['last_name'],
         "email": request.form['email']
-    }#the information is captured from the form, we created this dictionary, then we save the whole object
-    User.save(data)
+    }  # the information is captured from the form, we created this dictionary, then we save the whole object
+    User.save(data)  # I could pass request.form into save
     return redirect('/users')
 
 
 @app.route('/users')
 def users():
     return render_template('results.html', users=User.get_all())
-    #
 
+
+@app.route('/users/<int:id>')
+def show(id):
+    data = {
+        "id": id
+    }
+    user = User.get_one(data)
+    print(user.first_name)
+    return render_template('show.html', user=user)
+
+
+@app.route('/users/delete/<int:id>')
+def delete(id):
+    data = {
+        "id": id  # id goes into a dictionary called data
+    }
+    User.remove(data)
+    return redirect('/')
+
+@app.route('/users/update/<int:id>', methods=['POST'])
+def update(id):
+    data = {
+        "first_name": request.form['fname'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "id": id
+    }
+    User.update(data)
+    return redirect(f'/users/{id}')
+
+@app.route('/users/update/<int:id>', methods=['GET'])
+def editpage(id):
+    data = {
+        "id":id
+    }
+    return render_template('edit.html', user=data)
--- w2d3/users_CRUD_modularized/flask_app/models/user.py
+++ w2d3/users_CRUD_modularized/flask_app/models/user.py
@@ -13,15 +13,33 @@ class User:
 
     @classmethod
     def get_all(cls):
-            query = "SELECT * FROM users;"
-            users_from_db = connectToMySQL('users_schema').query_db(query)
-            users = []
-            for u in users_from_db:
-                users.append(cls(u))
-            return users
+        query = "SELECT * FROM users;"
+        users_from_db = connectToMySQL('users_schema').query_db(query)
+        users = []
+        for u in users_from_db:
+            users.append(cls(u))
+        return users
 
     @classmethod
     def save(cls,data):
-            query = "Insert INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());"
-            user_id = connectToMySQL('users_schema').query_db(query,data)
-            return user_id
+        query = "Insert INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());"
+        user_id = connectToMySQL('users_schema').query_db(query,data)
+        return user_id
+
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL('users_schema').query_db(query,data)
+        print(result)
+        #Use class to turn it into a class
+        return cls(result[0])
+
+    @classmethod
+    def remove(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
