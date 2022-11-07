fa87a681bc5ff9639a570b649c5fba0c610cc69e
Boyd Lever <boydlever@stanfordhacker.com>
2022-11-06 22:12:07 -0800

    added comments

--- w2d3/users_CRUD_modularized/server.py
+++ w2d3/users_CRUD_modularized/server.py
@@ -1,4 +1,4 @@
-from flask_app.controllers import users
+from flask_app.controllers import users #import users works here but not in other modularized assignments, including dojos and ninjas
 from flask_app import app
 # ...server.py
 
